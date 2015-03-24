47e1f5eba365dfb93d7997349ae92988eaa0636c
loganhuskins <loganghuskins@gmail.com>
2015-03-23 23:01:21 -0500

    Add email to the group table

---
+++ src/java/edu/corgi/uco/testUserCreation.sql
@@ -0,0 +1,10 @@
+/*
+    password: ppp
+*/
+
+insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values ('student@test.com', '123456789', 
+'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Student', 'Test');
+insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values ('admin@test.com', '987654321', 
+'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Admin', 'Test');
+insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values ('security@test.com', '741852963', 
+'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Security', 'Test');
