9f7f6a563f85c1771dbecfc9ffbe22f2c84f55f0
drenfro87 <drenfro87@gmail.com>
2015-03-30 13:46:21 -0500

    setup secretary structure, added email class,

--- src/java/edu/corgi/uco/testUserCreation.sql
+++ src/java/edu/corgi/uco/testUserCreation.sql
@@ -7,7 +7,7 @@ insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values (
 insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values ('admin@test.com', '987654321', 
 'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Admin', 'Test');
 insert into USER_TABLE (EMAIL, UCO_ID, PASSWORD, FIRST_NAME, LAST_NAME) values ('secretary@test.com', '741852963', 
-'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Security', 'Test');
+'c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7', 'Secretary', 'Test');
 
 insert into GROUP_TABLE (GROUPNAME, EMAIL) values ('student', 'student@test.com');
 insert into GROUP_TABLE (GROUPNAME, EMAIL) values ('admin', 'admin@test.com');
