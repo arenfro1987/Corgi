6785242d9a29d8e51c7f331e7b8aec3259a6c263
drenfro87 <drenfro87@gmail.com>
2015-04-01 00:06:33 -0500

    changed schema

--- src/java/edu/corgi/uco/schema.sql
+++ src/java/edu/corgi/uco/schema.sql
@@ -1,94 +1,104 @@
-drop table GROUP_TABLE;
-drop table MEETING;
-drop table PROPOSED_SCHEDULE;
-drop table COURSE_OFFERING;
-drop table COURSE_PREREQUISITE;
-drop table HAS_TAKEN;
-drop table COURSE;
-drop table USER_TABLE;
+drop table Course;
+drop table Appointment;
+drop table UserTable;
+drop table MajorCodes;
+drop table GroupTable;
+drop table Schedule;
+drop table CourseScheduleLinkage;
+drop table TakenCourses;
+drop table IsPreReq;
+drop table IsCoReq;
+drop table MajorReq;
 
-create table USER_TABLE (
-    USER_ID integer not null generated always 
+create table UserTable (
+    userID integer not null generated always 
         as identity (start with 1, increment by 1),
-    EMAIL varchar(50) unique not null,
-    UCO_ID varchar(10),
-    PASSWORD char(64) not null, /*SHA-256 Encrypted*/
-    FIRST_NAME varchar(20) not null,
-    LAST_NAME varchar(20) not null,
-    primary key(USER_ID)    
-);
-/*
-    studentgroup, secretarygroup, admingroup
-*/
-create table GROUP_TABLE (    
-    GROUPNAME varchar(20),
-    EMAIL varchar(50) not null,
-    primary key(GROUPNAME, EMAIL),
-    foreign key(EMAIL) references USER_TABLE(EMAIL)
+    email varchar(50) not null,
+    ucoID varchar(10),
+    password char(64) not null, /*SHA-256 Encrypted*/
+    firstName varchar(20) not null,
+    lastName varchar(20) not null
+    primary key(userID)    
 );
 
-create table COURSE (
-    DEPARTMENT varchar(4),
-    COURSE_NUMBER varchar(4),
-    primary key(DEPARTMENT, COURSE_NUMBER)
+create table Course(
+    hours integer(2),
+    courseID integer not null generated always 
+        as identity(start with 1, increment by 1)
+    dept varchar(30) not null,
+    course# integer(4) not null,
+    title varchar(200) not null,
+    semester varchar(6),
+    courseYear integer(4),
+    sec# integer(11)
+    primary key(courseID)
 );
 
-create table HAS_TAKEN (
-    USER_ID integer,
-    DEPARTMENT varchar(4),
-    COURSE_NUMBER varchar(4),
-    primary key(USER_ID, DEPARTMENT, COURSE_NUMBER),
-    foreign key(USER_ID) references USER_TABLE(USER_ID),
-    foreign key(DEPARTMENT, COURSE_NUMBER) references COURSE(DEPARTMENT, COURSE_NUMBER)
+create table Appointment(
+    appointmentID integer not null generated always
+        as identity(start with 1, increment by 1)
+    appointmentDate Date not null,
+    duration integer(3),
+    userID integer
+    primary key(appointmentID)
 );
 
-create table COURSE_OFFERING (
-    CRN varchar(10),
-    DEPARTMENT varchar(4),
-    COURSE_NUMBER varchar(4),
-    primary key(CRN, DEPARTMENT, COURSE_NUMBER),
-    foreign key(DEPARTMENT, COURSE_NUMBER) references COURSE(DEPARTMENT, COURSE_NUMBER)
+create table MajorCodes(
+    userID integer,
+    majorCode integer(4)
+    primary key(userID)
 );
 
-create table COURSE_PREREQUISITE (
-    PRE_DEPARTMENT varchar(4),
-    PRE_COURSE_NUMBER varchar(4),
-    POST_DEPARTMENT varchar(4),
-    POST_COURSE_NUMBER varchar(4),
-    primary key(PRE_DEPARTMENT, PRE_COURSE_NUMBER, POST_DEPARTMENT, POST_COURSE_NUMBER),
-    foreign key(PRE_DEPARTMENT, PRE_COURSE_NUMBER) references COURSE(DEPARTMENT, COURSE_NUMBER ),
-    foreign key(POST_DEPARTMENT, POST_COURSE_NUMBER) references COURSE(DEPARTMENT, COURSE_NUMBER)
-);
-/*
-    proposed schedule for the next semester
-*/
-create table PROPOSED_SCHEDULE (
-    USER_ID integer,
-    CRN varchar(10),
-    DEPARTMENT varchar(4),
-    COURSE_NUMBER varchar(4),
-    primary key(USER_ID, CRN),
-    foreign key(USER_ID) references USER_TABLE(USER_ID),
-    foreign key(CRN, DEPARTMENT, COURSE_NUMBER) references COURSE_OFFERING(CRN, DEPARTMENT, COURSE_NUMBER)
+create table GroupTable(
+    userID integer,
+    groupname varchar(50),
+    email varchar(50),
+    groupID integer not null generated always 
+        as identity (start with 1, increment by 1)
+    primary key(groupID)
 );
 
-
-create table MEETING (
-    MEETING_ID integer not null generated always 
+create table Schedule(
+    scheduleID integer not null generated always 
         as identity (start with 1, increment by 1),
-    MEETING_DATE date,
-    MEETING_TIME time,
-    PROFESSOR integer,
-    STUDENT integer,
-    primary key(MEETING_ID),
-    foreign key(PROFESSOR) references USER_TABLE(USER_ID),
-    foreign key(STUDENT) references USER_TABLE(USER_ID)
+    userID integer,
+    approved boolean,
+    holdRemoved boolean
+    primary key(scheduleID)
 );
 
-create table COMPLETED_STUDENTS (
-    COMPLETED_STUDENTS_ID INTEGER NOT NULL GENERATED ALWAYS
-        AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    STUDENT_USER_ID INTEGER NOT NULL,
-    PROSPOSED_SCHEDULE_ID INTEGER NOT NULL,
-    ACCEPTED_MEETING_ID INTEGER NOT NULL
+create table CourseScheduleLinkage(
+    userID integer,
+    courseID integer
+    primary key(userID, courseID)
+}
+
+create table TakenCourses(
+    courseID integer,
+    userID integer,
+    grade varchar(1)
+    primary key(courseID, userID)
 );
+
+create table IsPreReq(
+    mainCourseID  integer,
+    preReqCourseID integer
+    primary key(mainCourseID, preReqCourseID)
+);
+
+create table IsCoReq(
+    mainCourseID integer,
+    coReqCourseID integer
+    primary key(mainCourseID, coReqCourseID)
+);
+
+create table MajorReq(
+    majorCode integer(4),
+    courseID integer
+    primary key(majorCode, courseID)
+);
+    
+    
+    
+    
+/
